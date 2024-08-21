9dd1081b75aa2644422f559b2f640ff8312ecea0
PetrovAnton23 <PetrovAnton23>
2024-08-21 18:46:07 +0300

    Homework_5

--- lesson_5/lesson_5_task_3.3.py
+++ lesson_5/lesson_5_task_3.3.py
@@ -8,9 +8,9 @@ try:
     for a in range(5):
         blue_button = driver.find_element(
             "xpath", "//button[contains(concat(' ', normalize-space(@class), ' '), 'btn-primary')]").click()
-        blue_button.click()
+        blue_button
         sleep(2)
-        driver.switch_to.alert()
+        driver.switch_to.alert.accept()
 except Exception as ex:
     print(ex)
 finally:
