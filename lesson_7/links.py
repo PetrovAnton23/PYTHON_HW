45e843e6dc389e05359295e428b4dfd3aceba225
PetrovAnton23 <PetrovAnton23>
2024-08-24 10:00:04 +0300

    Домашняя работа_7

---
+++ lesson_7/links.py
@@ -0,0 +1,3 @@
+test_form_URL = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+calculator_URL = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+shop_URL = "https://www.saucedemo.com/"
